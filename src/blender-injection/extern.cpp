fc35455422dbfa45fea40bf65f872bc1b22412f8
Natsune Mochizuki <me@natsuneko.cat>
2023-05-09 14:16:04 +0900

    feat: pass-through all unhandled drop events

--- src/blender-injection/extern.cpp
+++ src/blender-injection/extern.cpp
@@ -3,24 +3,6 @@
 #include "BlenderObj.h"
 #include "BlenderPatcher.h"
 
-const std::vector<std::string> SUPPORTED_FORMATS = {
-    ".abc",
-    ".bvh",
-    ".fbx",
-    ".dae",
-    ".glb",
-    ".gltf",
-    ".obj",
-    ".ply",
-    ".stl",
-    ".svg",
-    ".usd",
-    ".usda",
-    ".usdc",
-    ".x3d",
-    ".wrl"
-};
-
 std::vector<std::intptr_t> isAlreadyTriggered = {};
 
 extern "C" void DropEventHookCallback(void* c, void* win, char* path)
@@ -32,36 +14,35 @@ extern "C" bool View3DImaEmptyDropPollHookCallback(bContext* c, wmDrag* drag, wm
 {
     const auto ptr = reinterpret_cast<intptr_t>(drag);
 
+    if (BlenderPatcher::GetInstance()->View3DImaDropPoll(c, drag, event))
+    {
+        auto ob = static_cast<int*>(BlenderPatcher::GetInstance()->EDView3dGiveObjectUnderCursor(c, event->mval));
+        if (ob == nullptr)
+            return true;
+
+        // NOTE: the pointed fields (value) have been determined, this cast is no problem.
+        if (reinterpret_cast<int>(ob + 0x000000E0) == /* OB_EMPTY */ 0 && reinterpret_cast<int>(ob + 0x000003FF) == /* OB_EMPTY_IMAGE */ 8) // NOLINT(clang-diagnostic-pointer-to-int-cast)
+            return true;
+    }
+
     if (drag->type == /* WM_DRAG_PATH */ 4)
     {
         const std::filesystem::path path(drag->path);
         const auto extension = path.extension();
 
-        if (const auto ref = std::ranges::find(SUPPORTED_FORMATS, extension); ref != std::end(SUPPORTED_FORMATS))
-        {
-            if (const auto itr = std::ranges::find(isAlreadyTriggered, ptr); itr != std::end(isAlreadyTriggered))
-                return false; // already triggered
-
-            isAlreadyTriggered.push_back(ptr);
-
-            const char* imports[] = {"bpy", nullptr};
-            const auto expression = R"(bpy.ops.object.drop_event_listener2("INVOKE_DEFAULT", filename=R")" + std::string(drag->path) + "\")";
-
-            BlenderPatcher::GetInstance()->RunStringEval(c, imports, expression.c_str());
+        if (const auto itr = std::ranges::find(isAlreadyTriggered, ptr); itr != std::end(isAlreadyTriggered))
             return false; // already triggered
-        }
+
+        isAlreadyTriggered.push_back(ptr);
+
+        const char* imports[] = {"bpy", nullptr};
+        const auto expression = R"(bpy.ops.object.drop_event_listener("INVOKE_DEFAULT", filename=R")" + std::string(drag->path) + "\")";
+
+        BlenderPatcher::GetInstance()->RunStringEval(c, imports, expression.c_str());
+        return false; // already triggered
+
     }
 
-    if (!BlenderPatcher::GetInstance()->View3DImaDropPoll(c, drag, event))
-        return false;
-
-    auto ob = static_cast<int*>(BlenderPatcher::GetInstance()->EDView3dGiveObjectUnderCursor(c, event->mval));
-    if (ob == nullptr)
-        return true;
-
-    // NOTE: the pointed fields (value) have been determined, this cast is no problem.
-    if (reinterpret_cast<int>(ob + 0x000000E0) == /* OB_EMPTY */ 0 && reinterpret_cast<int>(ob + 0x000003FF) == /* OB_EMPTY_IMAGE */ 8) // NOLINT(clang-diagnostic-pointer-to-int-cast)
-        return true;
 
     return false;
 }

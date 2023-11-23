9676232871be243ad16bca0ca7b1814d0147342e
MOCHIZUKI Natsune <me@natsuneko.cat>
2023-11-23 18:45:51 +0900

    chore: bump version to v2.12.0

--- src/blender-extension/__init__.py
+++ src/blender-extension/__init__.py
@@ -11,7 +11,7 @@ bl_info = {
     "author": "Natsuneko",
     "description": "Blender add-on for import some files from drag-and-drop",
     "blender": (3, 1, 0),
-    "version": (2, 11, 0),
+    "version": (2, 12, 0),
     "location": "Drag and Drop Support",
     "warning": "",
     "category": "Import-Export",
